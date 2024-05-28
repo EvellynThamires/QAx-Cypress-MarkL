5e0824d6d4d555c431cf694d63070bd753385db6
Evellyn Thamires <xe.uyemura@gmail.com>
2024-05-28 20:12:45 -0300

    adding new test case and adding variables

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,11 +2,13 @@
 
 describe('tasks', () => {
     it('should add a new task', () => {
+        const taskName = 'Read a book'
+
         cy.request({
             url: 'http://localhost:3333/helper/tasks',
             method: 'DELETE',
             body: {
-                name: 'Read a book'
+                name: taskName
             }
         }).then(response => {
             expect(response.status).to.eq(204)
@@ -14,9 +16,42 @@ describe('tasks', () => {
 
         cy.visit('http://localhost:3000')
         
-        cy.get('input[placeholder="Add a new Task"]').type('Read a book')
+        cy.get('input[placeholder="Add a new Task"]').type(taskName)
         cy.contains('button', 'Create').click()
     
-        cy.contains('main div p', 'Read a book').should('be.visible')
+        cy.contains('main div p', taskName).should('be.visible')
+    })
+
+    it('should not be able to add duplicated task', () => {
+        const task = {
+            name: 'Study',
+            is_done: false
+        }
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {
+                name: task.name
+            }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: task
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+        cy.visit('http://localhost:3000')
+        
+        cy.get('input[placeholder="Add a new Task"]').type(task.name)
+        cy.contains('button', 'Create').click()
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
     })
 })
